296d8d7fc663ea484e170df22e8c44ba302af783
AbiMani <mosotillo_noemi@hotmail.com>
2020-06-06 20:24:53 -0600

    control servicio web a medias

---
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/ConsultarCargaAcademicaActivity.java
@@ -0,0 +1,100 @@
+package ues.fia.eisi.reservalocalfia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ConsultarCargaAcademicaActivity extends AppCompatActivity {
+    ControlReserveLocal db;
+    static List<CargaAcademica> listaCargaAcademica;
+    static List<String> nombreCargaAcademica;
+    EditText cicloTxt;
+    ListView listViewCargaAcademica;
+    private final String urlLocal = "http://192.168.1.6/ws_consultar_cargaacademica.php";
+    private final String urlHostingGratuito = "https://bv12004pdm115.000webhostapp.com/ws_consultar_cargaacademica.php";
+    private String urlPublicoUES = "https://eisi.fia.ues.edu.sv/eisi25/MQ25001/ws_db_materia_fecha.php";
+
+    @SuppressLint("NewApi")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_carga_academica);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        db = new ControlReserveLocal(this);
+        listaCargaAcademica = new ArrayList<CargaAcademica>();
+        nombreCargaAcademica = new ArrayList<String>();
+        cicloTxt = (EditText) findViewById(R.id.editText_ciclo);
+        listViewCargaAcademica = (ListView) findViewById(R.id.listView1);
+    }
+
+    public void servicioPHP(View v) {
+        String ciclo = cicloTxt.getText().toString();
+        String url = "";
+        switch (v.getId()) {
+            case R.id.button1:
+                url = urlLocal + "?codigociclo=" + ciclo ;
+                break;
+            case R.id.button2:
+                url = urlHostingGratuito +"?codigociclo=" + ciclo ;
+                break;
+            case R.id.button4:
+                url = urlPublicoUES + "?codigociclo=" + ciclo ;
+                break;
+        }
+        String cargaacademicaExternas = ControladorServicio.obtenerRespuestaPeticion(url, this);
+        try {
+            listaCargaAcademica.addAll(ControladorServicio.obtenerCargaAcademicasExterno(cargaacademicaExternas, this));
+            actualizarListView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void guardar(View v) {
+        db.abrir();
+        for (int i = 0; i < listaCargaAcademica.size(); i++) {
+            Log.v("guardar", db.insertar(listaCargaAcademica.get(i)));
+        }
+        db.cerrar();
+        Toast.makeText(this, "Guardado con exito", Toast.LENGTH_LONG).show();
+        listaCargaAcademica.removeAll(listaCargaAcademica);
+        actualizarListView();
+    }
+
+    private void actualizarListView() {
+        String dato = "";
+        nombreCargaAcademica.clear();
+        for (int i = 0; i < listaCargaAcademica.size(); i++) {
+            dato = listaCargaAcademica.get(i).getcodigoAsignatura() + " " + listaCargaAcademica.get(i).getcarnetDocente()+ " " + listaCargaAcademica.get(i).getidRolDocente();
+            nombreCargaAcademica.add(dato);
+        }
+        eliminarElementosDuplicados();
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombreCargaAcademica);
+        listViewCargaAcademica.setAdapter(adaptador);
+    }
+
+    private void eliminarElementosDuplicados() {
+        HashSet<CargaAcademica> conjuntoCargaAca = new HashSet<CargaAcademica>();
+        conjuntoCargaAca.addAll(listaCargaAcademica);
+        listaCargaAcademica.clear();
+        listaCargaAcademica.addAll(conjuntoCargaAca);
+        HashSet<String> conjuntoNombre = new HashSet<String>();
+        conjuntoNombre.addAll(nombreCargaAcademica);
+        nombreCargaAcademica.clear();
+        nombreCargaAcademica.addAll(conjuntoNombre);
+    }
+}
--- app/src/main/java/ues/fia/eisi/reservalocalfia/ServiciosMenuActivity.java
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/ServiciosMenuActivity.java
@@ -2,7 +2,9 @@ package ues.fia.eisi.reservalocalfia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServiciosMenuActivity extends AppCompatActivity {
 
@@ -11,4 +13,36 @@ public class ServiciosMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios_menu);
     }
+
+    public void lanzarActividad(View v) {
+        Intent i = null;
+        switch (v.getId()) {
+            case R.id.button_sw1:
+                i = new Intent();
+                break;
+            case R.id.button_sw2:
+                i = new Intent(this,ConsultarCargaAcademicaActivity.class);
+                break;
+            case R.id.button_sw3:
+                i = new Intent();
+                break;
+            case R.id.button_sw4:
+                i = new Intent();
+                break;
+            case R.id.button_sw5:
+                i = new Intent();
+                break;
+            case R.id.button_sw6:
+                i = new Intent();
+                break;
+            case R.id.button_sw7:
+                i = new Intent();
+                break;
+            case R.id.button_sw8:
+                i = new Intent();
+                break;
+        }
+        if (i != null) startActivity(i);
+    }
+
 }
