ac0634a431e55df125b5c0c73e338b6cd04b8d51
javierAleman09 <javiieraleman09@gmail.com>
2020-06-07 21:25:50 -0600

    modificacion en relacion docente-rol

--- app/src/main/java/ues/fia/eisi/reservalocalfia/DocenteActualizarActivity.java
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/DocenteActualizarActivity.java
@@ -3,8 +3,12 @@ package ues.fia.eisi.reservalocalfia;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DocenteActualizarActivity extends Activity {
 
@@ -12,8 +16,12 @@ public class DocenteActualizarActivity extends Activity {
     EditText editCarnetDocente;
     EditText editNombreDocente;
     EditText editApellido;
-    EditText editRol;
+    //EditText editRol;
     EditText editNomEscuela;
+    Spinner spRol;
+    ArrayList<String> listaSpinnerRol;
+    ArrayList<RolDocente> listaSpinnerRolObjetos;
+    ControlReserveLocal conn;
 
     /** Called when the activity is first created. */
     @Override
@@ -25,16 +33,28 @@ public class DocenteActualizarActivity extends Activity {
         editCarnetDocente = (EditText) findViewById(R.id.editCarnetDocente);
         editNombreDocente = (EditText) findViewById(R.id.editNombreDocente);
         editApellido = (EditText) findViewById(R.id.editApellido);
-        editRol = (EditText) findViewById(R.id.editRol);
+        //editRol = (EditText) findViewById(R.id.editRol);
+        spRol=(Spinner) findViewById(R.id.spinner_docente_rol_insertar);
         editNomEscuela = (EditText) findViewById(R.id.editNomEscuela);
+
+        conn = new ControlReserveLocal(this);
+        listaSpinnerRolObjetos=conn.consultarTodosRol();
+        obtenerRol();
+
+        ArrayAdapter<CharSequence> adapterRol = new ArrayAdapter(this,android.R.layout.simple_spinner_item,listaSpinnerRol);
+        spRol.setAdapter(adapterRol);
+
     }
 
     public void actualizarDocente(View v) {
+
         Docente variable = new Docente();
         variable.setCarnetDocente(editCarnetDocente.getText().toString());
         variable.setNombreDocente(editNombreDocente.getText().toString());
         variable.setApellido(editApellido.getText().toString());
-        variable.setRol(editRol.getText().toString());
+        //variable.setRol(editRol.getText().toString());
+        String selRol=spRol.getSelectedItem().toString();
+        variable.setRol(selRol);
         variable.setNomEscuela(editNomEscuela.getText().toString());
 
         helper.abrir();
@@ -45,12 +65,20 @@ public class DocenteActualizarActivity extends Activity {
 
     }
 
+    private void obtenerRol() {
+        listaSpinnerRol = new ArrayList<String>();
+        listaSpinnerRol.add(" -- Seleccione -- ");
+        for (int i=0;i<listaSpinnerRolObjetos.size();i++){
+            listaSpinnerRol.add(listaSpinnerRolObjetos.get(i).getNomRolDocente());
+        }
+    }
+
     public void limpiarTexto(View v) {
 
         editCarnetDocente.setText("");
         editNombreDocente.setText("");
         editApellido.setText("");
-        editRol.setText("");
+        //editRol.setText("");
         editNomEscuela.setText("");
     }
 }
