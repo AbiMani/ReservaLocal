622f4139f3b00a11098aa87010d7009cb23ad859
jessyrc <yessy2015rive@gmail.com>
2020-06-08 17:52:04 -0600

    WS insertar escuela y consultar listado de reservas de eventos

---
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/ConsultarReservasActivity.java
@@ -0,0 +1,97 @@
+package ues.fia.eisi.reservalocalfia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ConsultarReservasActivity extends AppCompatActivity {
+
+    ControlReserveLocal db;
+    static List<ReservaEvento> listaReservas;
+    static List<String> nombreReservas;
+    EditText cicloTxt;
+    ListView listViewReservas;
+    private final String urlLocal = "http://192.168.43.198/reservas_query.php";
+    private final String urlHostingGratuito = "https://reservalocalfia04.000webhostapp.com/reservas_query.php";
+    private String urlPublicoUES = "https://eisi.fia.ues.edu.sv/eisi25/MQ25001/ws_db_materia_fecha.php";
+
+    @SuppressLint("NewApi")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_reservas);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        db = new ControlReserveLocal(this);
+        listaReservas = new ArrayList<ReservaEvento>();
+        nombreReservas = new ArrayList<String>();
+        listViewReservas = (ListView) findViewById(R.id.lstReservas);
+    }
+
+    public void servicioPHP(View v) {
+        String url = "";
+        switch (v.getId()) {
+            case R.id.button1:
+                url = urlHostingGratuito + "?";
+                break;
+            case R.id.button2:
+                url = urlPublicoUES+"?";
+                break;
+        }
+        String reservasExternas = ControladorServicio.obtenerRespuestaPeticion(url, this);
+        try {
+            listaReservas.addAll(ControladorServicio.obtenerReservasExterno(reservasExternas, this));
+            actualizarListView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void guardar(View v) {
+        db.abrir();
+        for (int i = 0; i < listaReservas.size(); i++) {
+            Log.v("guardar", db.insertar(listaReservas.get(i)));
+        }
+        db.cerrar();
+        Toast.makeText(this, "Guardado con exito", Toast.LENGTH_LONG).show();
+        listaReservas.removeAll(listaReservas);
+        actualizarListView();
+    }
+
+    private void actualizarListView() {
+        String dato = "";
+        nombreReservas.clear();
+        for (int i = 0; i < listaReservas.size(); i++) {
+            dato = listaReservas.get(i).getCodigoEscuela() + " " + listaReservas.get(i).getNombreEvento()+ " " + listaReservas.get(i).getCapacidadTotalEvento()
+                    + " " + listaReservas.get(i).getFechaReservaEvento();
+            nombreReservas.add(dato);
+        }
+        eliminarElementosDuplicados();
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombreReservas);
+        listViewReservas.setAdapter(adaptador);
+    }
+
+    private void eliminarElementosDuplicados() {
+        HashSet<ReservaEvento> conjuntoReservas = new HashSet<ReservaEvento>();
+        conjuntoReservas.addAll(listaReservas);
+        listaReservas.clear();
+        listaReservas.addAll(conjuntoReservas);
+        HashSet<String> conjuntoNombre = new HashSet<String>();
+        conjuntoNombre.addAll(nombreReservas);
+        nombreReservas.clear();
+        nombreReservas.addAll(conjuntoNombre);
+    }
+}
